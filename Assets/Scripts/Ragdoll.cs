afdb0c99425ca198054b0afd8176b13f86a9db06
Tony Le <letony@icloud.com>
2019-03-01 16:52:13 +1100

    Ragdoll Physics tutorial completed with some basic implementation of joints

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Ragdoll : MonoBehaviour {
+
+	Animator animator = null;
+	public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+	
+	public bool RagdollOn 
+	{
+		get { return !animator.enabled; }
+		set {
+			animator.enabled = !value;
+			foreach (Rigidbody r in rigidbodies)
+				r.isKinematic = !value;
+		}
+	}
+
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		//auto grab rbs?
+		rigidbodies = new List<Rigidbody>(GetComponentsInChildren<Rigidbody>());
+
+		//Set all connected rigidbodies on this object to kinematic 
+		//so that the forces don't build up when converted back to dynamic rbs
+		foreach (var r in rigidbodies)
+		{
+			r.isKinematic = true;
+		}
+	}
+	
+	void Update () {
+		//Ragdoll trigger test
+		// if (Input.GetKeyDown(KeyCode.Space))
+		// {
+		// 	RagdollOn = !RagdollOn;
+		// }
+	}
+}
---
+++ Assets/Scripts/RagdollTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class RagdollTrigger : MonoBehaviour {
+
+	new Collider collider;
+	new Rigidbody rigidbody;
+
+	void Awake()
+	{
+		collider = GetComponent<Collider>();
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			//Let go of wrecking ball!
+			rigidbody.isKinematic = !rigidbody.isKinematic;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Ragdoll ragdoll = col.gameObject.GetComponentInParent<Ragdoll>();
+		if (ragdoll != null)
+		{
+			//Trigger ragdoll and turn it back into a normal collider
+			ragdoll.RagdollOn = true;
+			collider.isTrigger = false;
+			rigidbody.mass = 100f;
+			rigidbody.AddForce(Vector3.back * 100000f);
+		}
+			
+	}
+}
