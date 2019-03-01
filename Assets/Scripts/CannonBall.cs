8da18658dafc148e4943d67ddd42d0027462a5a7
Tony Le <letony@icloud.com>
2019-03-01 13:45:24 +1100

    Rigidbody Dynamics Tutorial Completed

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonBall : MonoBehaviour {
+	public float forceOnFire = 300;
+	
+	bool fire = false;
+	bool canFire = true;
+
+	Rigidbody rb = null;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.isKinematic = true;
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown && canFire)
+		{
+			rb.isKinematic = false;
+			rb.AddForce(transform.forward * forceOnFire);
+			// canFire = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObject : MonoBehaviour 
+{
+	public Material awakeMaterial = null;
+	public Material sleepingMaterial = null;
+
+	Rigidbody rb = null;
+
+	bool wasSleeping = false;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		//Change colour based on whether the object is sleeping or not
+		if (rb.IsSleeping() && !wasSleeping && sleepingMaterial != null)
+		{
+			wasSleeping = true;
+			GetComponent<MeshRenderer>().material = sleepingMaterial;
+		}
+		if (!rb.IsSleeping() && wasSleeping && awakeMaterial != null)
+		{
+			wasSleeping = false;
+			GetComponent<MeshRenderer>().material = awakeMaterial;
+		}
+
+	}
+
+}
